5325a566e1139ea3573836d8cfef1816455deed2
Dalia <lovablechic77@aim.com>
2018-12-19 17:15:14 -0800

    update forms java

--- js/main.js
+++ js/main.js
@@ -311,12 +311,13 @@
 
 /** order form hide/unhide test **/
 
+
 function myFunction() {
-  var x = document.getElementById("orderform");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  var x = document.getElementById("myOrderform");
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
 
