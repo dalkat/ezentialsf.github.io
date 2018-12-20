aff655478bb9ecd241194d7417d6372718e37db7
Dalia <lovablechic77@aim.com>
2018-12-19 17:31:15 -0800

    javascript cleanup and tagline cleanup

--- js/main.js
+++ js/main.js
@@ -309,17 +309,6 @@
         });
     };
 
-/** order form hide/unhide test **/
-
-
-function myFunction() {
-  var x = document.getElementById("myOrderform");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
 
 
    /* Initialize
