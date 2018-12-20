5dde3f6858a10c061594728f0c37117178a09a3a
Dalia <lovablechic77@aim.com>
2018-12-19 17:09:48 -0800

    javascript hide/unhide order form

--- js/main.js
+++ js/main.js
@@ -309,6 +309,17 @@
         });
     };
 
+/** order form hide/unhide test **/
+
+function myFunction() {
+  var x = document.getElementById("orderform");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 
    /* Initialize
     * ------------------------------------------------------ */
